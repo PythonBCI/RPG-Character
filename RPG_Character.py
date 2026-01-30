6372aac870556fbe618024c2d5eeb5f5bedf83d2
Mike <michaelsabatino6501@gmail.com>
2026-01-30 14:18:27 -0500

    Add character creation function with validations
    Implement character creation function with validation checks.

---
+++ RPG_Character.py
@@ -0,0 +1,29 @@
+full_dot = '●'
+empty_dot = '○'
+
+
+def create_character(character_name, strength, intelligence, charisma):
+
+    character_name.str = 
+
+if not character_name:
+    print("The Character should have a name")
+    return None 
+
+if len(character_name) > 10:
+print("The character name is too long")
+return None 
+
+if ' ' in character_name:
+    print("The character name should not contain spaces")
+    return None
+
+strength = int(strength)
+intelligence = int(intelligence)
+charisma = int(charisma)
+
+if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+    return None
+
+if not all(x <= 4 for x in [strength, intelligence, charisma]):
+    return None
